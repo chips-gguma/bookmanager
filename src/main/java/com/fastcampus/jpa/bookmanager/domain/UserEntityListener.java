a478ae2614c7385dc881856110f0819cebdf080d
chips-gguma <ljygguma@gmail.com>
2021-12-27 17:53:04 +0900

    UserEntityListener 작성

--- src/main/java/com/fastcampus/jpa/bookmanager/domain/UserEntityListener.java
+++ src/main/java/com/fastcampus/jpa/bookmanager/domain/UserEntityListener.java
@@ -1,4 +1,25 @@
 package com.fastcampus.jpa.bookmanager.domain;
 
+import com.fastcampus.jpa.bookmanager.repository.UserHistoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.PreUpdate;
+
+@Component
 public class UserEntityListener {
+    @Autowired
+    private UserHistoryRepository userHistoryRepository;
+
+    @PreUpdate
+    public void preUpdate(Object o) {
+        User user = (User) o;
+
+        UserHistory userHistory = new UserHistory();
+        userHistory.setUserId(user.getId());
+        userHistory.setName(user.getName());
+        userHistory.setEmail(user.getEmail());
+
+        userHistoryRepository.save(userHistory);
+    }
 }
